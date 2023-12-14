3a610bdddfa31489ee3867e7e4d9ce80c701eabc
makedonzp <95443399+makedonzp@users.noreply.github.com>
2023-12-14 21:00:08 +0300

    Add files via upload

---
+++ Ajax_task_1/task_1_ajax.js
@@ -0,0 +1,45 @@
+/*
+Задание 1.
+
+Вам дана заготовка и результат, который вы должны получить. Ваша задача — написать код, который будет преобразовывать XML в JS-объект и выводить его в консоль.
+
+*/
+let xmlString = `<?xml version="1.0" encoding="UTF-8" ?>
+<list>
+  <student>
+    <name lang="en">
+      <first>Ivan</first>
+      <second>Ivanov</second>
+    </name>
+    <age>35</age>
+    <prof>teacher</prof>
+  </student>
+  <student>
+    <name lang="ru">
+      <first>Петр</first>
+      <second>Петров</second>
+    </name>
+    <age>58</age>
+    <prof>driver</prof>
+  </student>
+</list>`;
+
+const domParser = new DOMParser();
+
+const xmlDOM = domParser.parseFromString(xmlString, "text/xml");
+
+let list = [];
+
+const student = xmlDOM.querySelectorAll("student");
+
+student.forEach((elem) => {
+  list.push({
+    name: `${elem.querySelector("first").textContent} ${
+      elem.querySelector("second").textContent
+    }`,
+    age: elem.querySelector("age").textContent,
+    prof: elem.querySelector("prof").textContent,
+    lang: elem.querySelector("name").getAttribute("lang"),
+  });
+});
+console.log(list);
---
+++ Ajax_task_5/task_5_ajax.js
@@ -0,0 +1,73 @@
+/*
+Задание 5.
+
+Написать код приложения, интерфейс которого состоит из двух input и кнопки. В input можно ввести любое число.
+
+Заголовок первого input — «номер страницы».
+Заголовок второго input — «лимит».
+Заголовок кнопки — «запрос».
+При клике на кнопку происходит следующее:
+
+Если число в первом input не попадает в диапазон от 1 до 10 или не является числом — выводить ниже текст «Номер страницы вне диапазона от 1 до 10»;
+Если число во втором input не попадает в диапазон от 1 до 10 или не является числом — выводить ниже текст «Лимит вне диапазона от 1 до 10»;
+Если и первый, и второй input не в диапазонах или не являются числами — выводить ниже текст «Номер страницы и лимит вне диапазона от 1 до 10»;
+Если числа попадают в диапазон от 1 до 10 — сделать запрос по URL https://jsonplaceholder.typicode.com/photos?_page=0&_limit=5, где GET-параметр _page — это число из первого input, а GET-параметр _limit — это введённое число второго input.
+Пример. Если пользователь ввёл 5 и 7, то запрос будет вида https://jsonplaceholder.typicode.com/photos?_page=5&_limit=7
+После получения данных вывести список картинок на экран.
+
+Если пользователь перезагрузил страницу, то ему должны показываться картинки из последнего успешно выполненного запроса (использовать localStorage).
+
+*/
+
+const letterNumber = document.querySelector(".input_letter_number");
+const letterLimit = document.querySelector(".input_letter_limit");
+const btnRequest = document.querySelector(".btn_request");
+const outText = document.querySelector(".out_text");
+const imgResult = document.querySelector(".image_from_server");
+
+let answer = localStorage.getItem("lastResult")
+  ? JSON.parse(localStorage.getItem("lastResult"))
+  : [];
+render(answer);
+btnRequest.addEventListener("click", () => {
+  if (
+    Number(letterNumber.value) == "" ||
+    (Number(letterLimit.value) == "", Number(letterNumber.value) <= 0) ||
+    Number(letterLimit.value) <= 0
+  ) {
+    outText.innerHTML = `вы ввели либо "0" либо буквы, нужно число в диапазоне 1-10`;
+    imgResult.innerHTML = "";
+  } else if (
+    Number(letterNumber.value) >= 1 &&
+    Number(letterLimit.value) >= 1 &&
+    Number(letterNumber.value) <= 10 &&
+    Number(letterLimit.value) <= 10
+  ) {
+    fetch(
+      `https://jsonplaceholder.typicode.com/photos?_page=${Number(
+        letterNumber.value
+      )}&_limit=${Number(letterLimit.value)}`
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        answer = data;
+        render(answer);
+      });
+    outText.innerHTML = "в диапазоне от 0-10";
+  } else {
+    letterNumber.value = "";
+    letterLimit.value = "";
+  }
+});
+
+function render(data) {
+  imgResult.innerHTML = "";
+  letterNumber.value = "";
+  letterLimit.value = "";
+  for (let i = 0; i < data.length; i++) {
+    imgResult.innerHTML += `<img src="${data[i].thumbnailUrl}" alt="image" />`;
+  }
+  localStorage.setItem("lastResult", JSON.stringify(data));
+}
